5e7bf9aa6c9a8aeeae1fc083c9e7eb23dcd6c75a
pepper-sister <yuyu7123@naver.com>
2025-10-25 00:45:39 +0900

    feat: 사용자 입력 부분

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import getCarArray from './utils/getCarArray.js';
+import getTryCount from './utils/getTryCount.js';
+
 class App {
-  async run() {}
+    async run() {
+        const carArray = await getCarArray();
+        const tryCount = await getTryCount();
+    }
 }
 
 export default App;
