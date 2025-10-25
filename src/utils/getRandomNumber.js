b72bf60a9f7e75bc638d7025a7f2d11b277a24cf
pepper-sister <yuyu7123@naver.com>
2025-10-25 11:13:43 +0900

    fix: 동기함수로 변경

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,7 +1,7 @@
 import { Random } from '@woowacourse/mission-utils';
 import { RANDOM_MIN, RANDOM_MAX, FORWARD_CONDITION } from '../constants/randomNumber.js';
 
-export default async function getRandomNumber() {
+export default function getRandomNumber() {
     const randomNumber = Random.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
 
     return randomNumber >= FORWARD_CONDITION;
