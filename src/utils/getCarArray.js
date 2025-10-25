7adc10ea461472912af63a92e4000cc6b3f6f9a1
pepper-sister <yuyu7123@naver.com>
2025-10-25 11:52:17 +0900

    feat: 이름 사이 띄어쓰기 무시

--- src/utils/getCarArray.js
+++ src/utils/getCarArray.js
@@ -3,6 +3,7 @@ import { INPUT_CAR_NAMES } from '../constants/message.js';
 
 export default async function getCarArray() {
     const carNames = await Console.readLineAsync(INPUT_CAR_NAMES);
+    const carArray = carNames.split(',').map((name) => name.trim());
 
-    return carNames.split(',');
+    return carArray;
 }
